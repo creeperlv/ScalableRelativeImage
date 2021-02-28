254e1ce8ede056648aef12b0a6c7712eaedd0748
Creeper Lv <creeperlv38@outlook.com>
2021-02-28 13:32:56 +0800

    Added a shape that will never be rendered called `Group` to help organize the SRI file and improve readability.

---
+++ ScalableRelativeImage/Nodes/Group.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScalableRelativeImage.Nodes
+{
+    public class Group : GraphicNode
+    {
+        public List<INode> Children=new();
+        public void RemoveChildAt(int i)
+        {
+            Children.RemoveAt(i);
+        }
+        public void RemoveChild(INode n)
+        {
+            Children.Remove(n);
+        }
+        public override List<INode> ListNodes()
+        {
+            return Children;
+        }
+        public override void AddNode(INode node)
+        {
+            Children.Add(node);
+        }
+        public override void Paint(ref Graphics TargetGraphics, RenderProfile profile)
+        {
+            foreach (var item in Children)
+            {
+                if(item is GraphicNode)
+                {
+                    ((GraphicNode)item).Paint(ref TargetGraphics, profile);
+                }
+            }
+        }
+    }
+}
